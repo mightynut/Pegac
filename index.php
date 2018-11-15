c61d680af2c272f81f55b383329b2c12c30100a6
mightynut <43552735+mightynut@users.noreply.github.com>
2018-11-15 02:39:10 +0200

    Add Files

---
+++ index.php
@@ -0,0 +1,7 @@
+<? include $_SERVER['DOCUMENT_ROOT'] . "/layout/head.phtml"?>
+
+<? include $_SERVER['DOCUMENT_ROOT'] . "/layout/header.phtml"?>
+
+<? include $_SERVER['DOCUMENT_ROOT'] . "/page/main.phtml"?>
+
+<? include $_SERVER['DOCUMENT_ROOT'] . "/layout/footer.phtml"?>
